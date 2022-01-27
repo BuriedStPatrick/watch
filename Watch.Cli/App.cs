a1f28468d1651df07c9469af1bf66077815e13d6
Patrick Christensen <patrick.christensen@eksponent.com>
2022-01-27 12:42:08 +0100

    Initial commit

---
+++ Watch.Cli/App.cs
@@ -0,0 +1,43 @@
+using Terminal.Gui;
+
+namespace Watch.Cli;
+
+public sealed class App : Toplevel
+{
+    public static TimeSpan UpdateFrequency { get; set; }
+    public static string[] Args { get; set; }
+
+    public App()
+    {
+        var view = new View
+        {
+            X = 0,
+            Y = 1,
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+
+        var label = new Label("Output");
+        view.Add(label);
+
+        Add(view);
+
+        Application.MainLoop.AddTimeout(UpdateFrequency, _ =>
+        {
+            using var pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = Args[0];
+            pProcess.StartInfo.Arguments = string.Join(" ", Args[1..]); // any additional arguments
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+            pProcess.Start();
+            var output = pProcess.StandardOutput.ReadToEnd(); //The output result
+            pProcess.WaitForExit();
+
+            label.Text = output;
+
+            return true;
+        });
+    }
+}
---
+++ Watch.Cli/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Terminal.Gui;
+using Watch.Cli;
+
+public static class Program
+{
+    public static Task<int> Main(params string[] args)
+    {
+        App.UpdateFrequency = TimeSpan.FromSeconds(1);
+        App.Args = args;
+
+        Application.Run<App>();
+
+        return Task.FromResult(0);
+    }
+}
